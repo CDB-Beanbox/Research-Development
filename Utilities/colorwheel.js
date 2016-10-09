fe7a66ab5c48bce2b29decf281dcc497e6c84c7a
cdb-pf <clreed13@gmail.com>
2016-10-09 12:17:57 -0400

    colorwheel
    simple decode of text colors.

---
+++ Utilities/colorwheel.js
@@ -0,0 +1,8 @@
+function(context, args)
+{
+var a = "`1one` `2two` `3three` `4four` `5five` `6six` `7seven` `8eight` `9nine` `0ten` !bang! -minus- +plus+ \n"
+var b = "`aa` `bb` `cc` `dd` `ee` `ff` `hh` `ii` `jj` `kk` `ll` `mm` `nn` `oo` `pp` `qq` `rr` `ss` `tt` `uu`  `vv` `ww` `xx` `yy` `zz` \n"
+var c = "`AA` `BB` `CC` `DD` `EE` `FF` `GG` `HH` `II` `JJ` `KK` `LL` `MM` `NN` `OO` `PP` `QQ` `RR` `SS` `TT` `UU` `VV` `WW``XX` `YY` `ZZ` \n"
+var blank = "\n"
+	return blank + b + c + a;
+}
